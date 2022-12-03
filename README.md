03381337ecba3e139a0c37287452082a864a8eb9
Raj Changappa <rajchangappa1@gmail.com>
2022-12-03 17:04:23 +0530

    program logic update and styling changes

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# Rock_paper_scissors_game
-Rock paper scissors  web game 
+# ROCK PAPER SCISSORS
+
+**Description** : Rock paper scissors is a hand game usually played between two people, in which each player simultaneously forms one of three shapes with an outstretched hand. These shapes are "rock", "paper", and "scissors". This Mini Project is aimed at automating one of the player called as Computerized Player and the taking rock, paper or scissor as an input from the used.
+
+**Created by** - github.com/whorajK
+
+**Introduction**
+Rock paper scissors (also known by other orderings of the three items, with "rock" sometimes being called "stone", roshambo or ro-sham-bo) is a hand game usually played between two people, in which each player simultaneously forms one of three shapes with an outstretched hand. These shapes are "rock" (a closed fist), "paper" (a flat hand), and "scissors" (a fist with the index finger and middle finger extended, forming a V). "Scissors" is identical to the two-fingered V sign (also indicating "victory" or "peace") except that it is pointed horizontally instead of being held upright in the air.
+
+A simultaneous, zero-sum game, it has only two possible outcomes: a draw, or a win for one player and a loss for the other. A player who decides to play rock will beat another player who has chosen scissors ("rock crushes scissors" or sometimes "blunts scissors"), but will lose to one who has played paper ("paper covers rock"); a play of paper will lose to a play of scissors ("scissors cuts paper"). If both players choose the same shape, the game is tied and is usually immediately replayed to break the tie. Read More on Wiki Pedia
+
+**Game Play**
+The players may count aloud to three, or speak the name of the game (e.g. "Rock! Paper! Scissors!"), either raising one hand in a fist and swinging it down with each syllable or holding it behind their back. They then "throw" by extending it towards their opponent. Variations include a version where players throw immediately on the third count (thus throwing on the count of "Scissors!"), or a version where they shake their hands three times before "throwing". We are going to computerize this game play, so that computer will choose some random choice while the player will have the choice to choose one [Rock! Paper! or Scissors! ]
+
+**Rules**
+A player who decides to play rock will beat another player who has chosen scissors ("rock crushes scissors" or sometimes "blunts scissors"), but will lose to one who has played paper ("paper covers rock"); a play of paper will lose to a play of scissors ("scissors cuts paper"). If both players choose the same shape, the game is tied and is usually immediately replayed to break the tie.
+
+Possible cases -
+
+if (player == rock) and (computer == paper); computer wins
+
+if (player == paper) and (computer == paper); tie
+
+if (player == scissors) and (computer == paper); player wins
+
+if (player == rock) and (computer == rock); tie
+
+if (player == paper) and (computer == rock); player wins
+
+if (player == scissors) and (computer == rock); computer wins
+
+if (player == rock) and (computer == scissors); player wins
+
+if (player == paper) and (computer == scissors); computer wins
+
+if (player == scissors) and (computer == scissors); tie
+
+**Dependencies**
+Html
+Css
+Javascript
--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-const CHOICES = ["rock", "paper", "scissors"];
-let playerChoices = document.querySelectorAll(".choice_btn");
+// VARIABLES
+const CHOICES = ["✊", "🖐️", "✌️"];
+const playerChoices = document.querySelectorAll(".choice_btn");
 
 const playerScoreOutput = document.querySelector(".player_score");
 const computerScoreOutput = document.querySelector(".computer_score");
@@ -7,101 +8,104 @@ const computerScoreOutput = document.querySelector(".computer_score");
 const playerChoiceOutput = document.querySelector(".player_choice");
 const computerChoiceOutput = document.querySelector(".computer_choice");
 
+const modalBtn = document.querySelector(".modal_btn");
+const modal = document.querySelector(".modal");
+const modalText = document.querySelector(".modal__text");
+
 let playerScore = 0;
 let computerScore = 0;
 
 // GET PLAYER INPUT
-playerChoices.forEach((choice) => {
+playerChoices.forEach((choice) =>
 	choice.addEventListener("click", (event) => {
 		playerChoiceOutput.textContent = choice.textContent;
 		let playerSelectedChoice = playerChoiceOutput.textContent;
 
 		generateRandomComputerChoice(playerSelectedChoice);
-	});
-});
+	})
+);
 
 // GENERATE RANDOM COMPUTER OUTPUT
 function generateRandomComputerChoice(playerSelectedChoice) {
-	computerChoiceOutput.textContent =
-		CHOICES[Math.floor(Math.random() * CHOICES.length)];
+	computerChoiceOutput.textContent = CHOICES[Math.floor(Math.random() * 3)];
 	let computerGeneratedChoice = computerChoiceOutput.textContent;
 
-	getWinner(playerSelectedChoice, computerGeneratedChoice);
+	updateScore(playerSelectedChoice, computerGeneratedChoice);
 }
 
-// GET WINNER
-function getWinner(playerSelectedChoice, computerGeneratedChoice) {
-	let winner;
+// UPDATE SCORE AND UPDATE ROUND MESSAGE
+function updateScore(playerSelectedChoice, computerGeneratedChoice) {
+	let winner = "";
 
 	if (
-		(playerSelectedChoice === "rock" && computerGeneratedChoice === "paper") ||
-		(playerSelectedChoice === "paper" &&
-			computerGeneratedChoice === "scissors") ||
-		(playerSelectedChoice === "scissors" && computerGeneratedChoice === "rock")
+		(playerSelectedChoice === "✊" && computerGeneratedChoice === "🖐️") ||
+		(playerSelectedChoice === "🖐️" && computerGeneratedChoice === "✌️") ||
+		(playerSelectedChoice === "✌️" && computerGeneratedChoice === "✊")
 	) {
-		winner = "computer";
+		if (computerScore <= 5 || playerScore <= 5) {
+			winner = "computer";
+			computerScore++;
+			computerScoreOutput.textContent = `COMPUTER: ${computerScore}`;
+		} else {
+			return;
+		}
 	} else if (
-		(computerGeneratedChoice === "rock" && playerSelectedChoice === "paper") ||
-		(computerGeneratedChoice === "paper" &&
-			playerSelectedChoice === "scissors") ||
-		(computerGeneratedChoice === "scissors" && playerSelectedChoice === "rock")
+		(computerGeneratedChoice === "✊" && playerSelectedChoice === "🖐️") ||
+		(computerGeneratedChoice === "🖐️" && playerSelectedChoice === "✌️") ||
+		(computerGeneratedChoice === "✌️" && playerSelectedChoice === "✊")
 	) {
-		winner = "player";
+		if (computerScore <= 5 || playerScore <= 5) {
+			winner = "player";
+			playerScore++;
+			playerScoreOutput.textContent = `PLAYER: ${playerScore}`;
+		} else {
+			return;
+		}
 	} else {
 		winner = "";
+		computerScore = computerScore;
+		playerScore = playerScore;
 	}
 
-	displayRoundResult(winner, playerSelectedChoice, computerGeneratedChoice);
-}
-// DISPLAY ROUND RESULTS
-function displayRoundResult(
-	winner,
-	playerSelectedChoice,
-	computerGeneratedChoice
-) {
-	const resultTextTitle = document.querySelector(".main__h2");
-	const resultTextSubtitile = document.querySelector(".main__p");
+	const mainTextTitle = document.querySelector(".main__h2");
+	const mainTextSubtitle = document.querySelector(".main__p");
 
-	if (winner === "player") {
-		resultTextTitle.textContent = "player wins";
-		resultTextSubtitile.textContent = `${playerSelectedChoice} beats ${computerGeneratedChoice}`;
-	} else if (winner === "computer") {
-		resultTextTitle.textContent = "computer wins";
-		resultTextSubtitile.textContent = `${playerSelectedChoice} looses to ${computerGeneratedChoice}`;
+	if (winner === "") {
+		mainTextTitle.textContent = "No one wins !";
+		mainTextSubtitle.textContent = "Both selections are the same";
 	} else {
-		resultTextTitle.textContent = "draw";
-		resultTextSubtitile.textContent = `${playerSelectedChoice} is same as ${computerGeneratedChoice}`;
-	}
-	updateScores(winner);
-}
+		mainTextTitle.textContent = `${winner} wins !`;
 
-// UPDATE SCORES
-function updateScores(winner) {
-	playerScoreOutput.textContent = `PLAYER: ${playerScore}`;
-	computerScoreOutput.textContent = `COMPUTER: ${computerScore}`;
-
-	if (playerScore === 5 || computerScore === 5) {
-		return openModal();
-	} else {
 		if (winner === "player") {
-			playerScore++;
+			mainTextSubtitle.textContent = `${playerSelectedChoice} beats ${computerGeneratedChoice}`;
 		} else if (winner === "computer") {
-			computerScore++;
+			mainTextSubtitle.textContent = `${computerGeneratedChoice} beats ${playerSelectedChoice}`;
 		} else {
-			playerScore;
-			computerScore;
+			return;
 		}
 	}
+
+	stopGame(playerScore, computerScore);
 }
 
-function openModal() {
-	document.body.style.backgroundColor = "black";
-	restartGame();
+// STOP GAME AND OPEN MODAL
+function stopGame(playerScore, computerScore) {
+	if (playerScore === 5 || computerScore === 5) {
+		playerChoices.forEach((choice) => {
+			choice.classList.add("control_off");
+		});
+
+		modal.classList.add("active");
+		modalBtn.addEventListener("click", (e) => {
+			modal.classList.remove("active");
+			restartGame();
+		});
+	} else {
+		return;
+	}
 }
+
 // RESTART GAME
 function restartGame() {
-	document.body.style.backgroundColor = "white";
-
-	playerScore = 0;
-	computerScore = 0;
+	location.reload();
 }
